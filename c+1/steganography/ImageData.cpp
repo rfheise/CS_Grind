6b266e374afd0e4126ef995f8c2e09e6cba5a3ca
rfheise <rfheise@comcast.net>
2021-04-30 04:22:07 +0000

    whale go brrrrr

--- c+1/steganography/ImageData.cpp
+++ c+1/steganography/ImageData.cpp
@@ -7,34 +7,45 @@ using namespace std;
 typedef uint8_t byte;
 
 ImageData::ImageData(string filename) {
-    image = ifstream(filename, ios::binary);
+    //open image for binary file io
+    image.open(filename, ios::binary);
+    //checks to make sure image exists
     if(!image.is_open()) {
         image.close();
         cout << "Error opening file" << endl;
         throw 1;
     }
+    //skip first chunk of bmp metadata
     image.seekg(14, ios::beg);
+    //read in size of head meta data
     head_size = 0;
     image.read(reinterpret_cast<char *>(&head_size), 4);
+    //get image width and height in pixels
     int height = 0;
     int width = 0;
     image.read(reinterpret_cast<char *>(&height), 4);
     image.read(reinterpret_cast<char *>(&width), 4);
+    //set file location to after meta data
     image.seekg(14+head_size, ios::beg);
+    //save size of "usable data" in bytes
+    //each pixel is 3 bytes
     filesize = height * width * 3;
+    //initialize reading buffer
     buffer = new char[buffer_size];
 
 }
+//adds meta data to bmp
+//rn just adds size of data being stored
 void ImageData::addMetaData(int meta) {
+    //copies inital bmp meta data to new bmp
     image.seekg(0, ios::beg);
     image.read(buffer, head_size + 14);
     out.write(buffer, head_size + 14);
+    //converts length integer to an array of bytes
     char *temp = (char *)&meta;
     for (unsigned int i = 0; i < sizeof(int); i++) {
+        //stores each byte in image
         writeCharacter(temp[i]);
-    }
-    for (int i = 0; i < 4; i++) {
-        cout << std::hex <<(unsigned int)temp[i] << endl;
     }
 }
 void ImageData::writeCharacter(char c) {
@@ -43,92 +54,160 @@ void ImageData::writeCharacter(char c) {
     for (int i = 0; i < 4; i++) {
         //isolate last two bits
         arr[i] &= c;
-        //shift c by 2 to get next two bits
+        //shift c by 2 to get next two bits in char
         c >>= 2;
     }
+    //read in one characters worth of storage
     image.read(buffer, 4);
     for (int i =0; i < 4; i++) {
+        //set last two bits of byte to image data
+        //sets last two bits of byte in buffer to 0
         buffer[i] &= 0xfc;
+        //sets last two bits of buffer byte to two bits in the character
         buffer[i] |= arr[i];
     }
+    //writes out edited buffer to outfile
     out.write(buffer, 4);
 }
-void ImageData::encrypt(string message) {
+//public method thta encrypts a string into bmp
+void ImageData::encrypt(string message, string outfile) {
+    //gets number of bytes you can store
+    //since we are storing the meta data in the first 16 bits we subtract
+    //it from space left
+    //since we are storing data in the 2 least significant bits
+    //it will take 4 bytes of the image file to store one byte of the data we want to store
     int allowed = (filesize - 16)/4;
-    if (message.size() > (unsigned int) allowed) {
+    //if size of message is larger than allowed throw an error
+    int length = message.size();
+    if (length > allowed) {
         cout << "message too large" << endl;
         throw 2;
     }
-    int length = message.size();
-    out = ofstream("out.bmp",ios::binary);
+    //open bitmap for writing to outfile
+    out.open(outfile,ios::binary);
+    //make sure file opened properly
+    if (!out.is_open()) {
+        cout << "Error writing to file" << endl;
+        throw 3;
+    }
+    //add meta data to new bmp
     addMetaData(length);
     for (int i = 0; i < length; i++) {
+        //write each new character to new file
         writeCharacter(message[i]);
     }
     out << image.rdbuf();
 
 }
+//gets size of a file
 int getSize(ifstream &fp) {
+    //stores current position
     int cur = fp.tellg();
+    //gets starting position
     fp.seekg(0,ios::beg);
     int beg = fp.tellg();
     fp.seekg(0,ios::end);
+    //gets ending pos
     int end = fp.tellg();
+    //resets it back to current position
     fp.seekg(cur, ios::beg);
+    //returns number of bytes in file
     return end - beg;
 }
-void ImageData::encrypt(ifstream &fp) {
+void ImageData::encrypt(ifstream &fp, string outfile) {
+      //gets number of bytes you can store
+    //since we are storing the meta data in the first 16 bits we subtract
+    //it from space left
+    //since we are storing data in the 2 least significant bits
+    //it will take 4 bytes of the image file to store one byte of the data we want to store
     int allowed = (filesize - 16)/4;
-    cout << allowed << endl;
+    //gets size of data file
     unsigned int size = getSize(fp);
-    cout << size << endl;
+    //if data file is larger than what you can store throw an error
     if (size > (unsigned int) allowed) {
         cout << "message too large" << endl;
         throw 2;
     }
+    //set file position to begining
     fp.seekg(0,ios::beg);
-    out = ofstream("out.bmp",ios::binary);
+    //open output file
+    out.open(outfile,ios::binary);
+    if (!out.is_open()) {
+        cout << "Error opening outfile" << endl;
+        throw 3;
+    }
+    //adds meta data to file
     addMetaData(size);
     char c;
+    //reads byte by byte from data file
     for (unsigned int i = 0; i < size; i++) {
         fp.read(&c, 1);
+        //writes new byte to output file
         writeCharacter(c);
     }
+    //after data is finished adding
+    //copy rest of the image to new file
     out << image.rdbuf();
 }
+//reads a character from the image file
+//used in decryption
 char ImageData::readCharacter() {
     char x = 0;
+    //reads in 4 bytes
     image.read(buffer, 4);
     for (int i = 3; i >= 0; i--) {
+        //starts at last byte since data is stored "backwards"
+        //right shitfs it to make room for next two bytes
         x <<= 2;
+        //saves last two bits only from buffer and adds them to 
+        //end of the character
         x |= (buffer[i] & 3); 
     }
+    //returns character read in
     return x;
 }
+//reads meta data
 int ImageData::readMetaData() {
     char meta[4];
+    //reads in meta data by reading in first 4
+    //stored bytes
     for (int i = 0; i < 4; i++) {
         meta[i] = readCharacter();
     }
-    // for (int i = 0; i < 4; i++) {
-    //     cout << std::hex <<(unsigned int)meta[i] << endl;
-    // }
+    //casts 4 byte array to integer
     return *((int *)(meta));
 
 }
+//decrypts data from image
 void ImageData::decrypt() {
-    cout << "here" << endl;
+    //reads metadata
     int length = readMetaData();
-    // int length = 55359;
-    cout << length << endl;
-    cout << "here" << endl;
+    //displayes each decrypted character
     for (int i = 0; i < length; i++) {
         cout << readCharacter();
     }
     cout << endl;
 }
+void ImageData::decrypt(string filename) {
+    //opens output file to put decrypted data in
+    //opens output file
+    out.open(filename);
+    if (!out.is_open()) {
+        cout << "Error reading data" << endl;
+        throw 3;
+    }
+    //reads in length of data
+    int length = readMetaData();
+    //writes each decrypted character to file
+    for (int i = 0; i < length; i++) {
+        out << readCharacter();
+    }
+}
+//image reader deconstructor
 ImageData::~ImageData() {
+    //closes image and output file
     image.close();
     out.close();
+    //frees buffer data
     delete[] buffer;
 }
--- c+1/steganography/ImageData.h
+++ c+1/steganography/ImageData.h
@@ -8,21 +8,37 @@ typedef uint8_t byte;
 
 class ImageData {
     private:
+        //image you are reading from
         ifstream image;
         int filesize;
+        //file you are writing to
         ofstream out;
+        //buffer
         char *buffer;
-        const int buffer_size = 10000;
+        //buffer size
+        const int buffer_size = 1000;
+        //method for adding meta data to bmp
         void addMetaData(int meta);
+        //method for reading characters from encrypted bmp
         char readCharacter();
+        //method for writing encrypted characters to bmp
         void writeCharacter(char c);
+        //method for reading in meta data from encrypted bmp
         int readMetaData();
+        //size of bmp 2nd chunk of meta data
         int head_size;
     public:
-        void encrypt(string message);
+        //string encryption method
+        void encrypt(string message,string outfile);
+        //stdio decryption method
         void decrypt();
+        //file decryption method
+        void decrypt(string outfile);
+        //constructor
         ImageData(string file);
-        void encrypt(ifstream &fp);
+        //file encryption method
+        void encrypt(ifstream &fp, string outfile);
+        //destructor
         ~ImageData();
 };
 
